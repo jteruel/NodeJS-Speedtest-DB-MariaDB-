76a88cd83e1a832ccf614569f1161ed36ade866c
Mike <jeffreyteruel@gmail.com>
2019-01-05 17:44:30 +0800

    Remove apps. Focus on index.js

--- apps/users.js
+++
@@ -1,1 +0,0 @@
-connection = require('./connection.js');
--- index.js
+++ index.js
@@ -2,22 +2,41 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-connection = require('./connection.js');
-var records = require('./apps/records.js');
 var speedTest = require('speedtest-net');
-var test = speedTest({maxTime: 5000}); 
+var test = speedTest({maxTime: 5000});  
+connection = require('./connection.js');
 
 app.set('view engine', 'ejs');
 // routing
-app.get('/', function (req, res, records) {
-	console.log(records.records);
-	//res.render('pages/index', records.records);
+app.get('/', function (req, res) {
+	connection.connect(function(err) {
+		if (err) throw err; 
+		connection.query("SELECT * FROM records ORDER BY date DESC", function (err, rows, fields) {
+    		if (err) throw err; 
+    		res.render('pages/index', { results:rows });
+  		});
+    });  
 });
 
 app.get('/get-record', function(req, res) {
 
-	//
+	test.on('data', data => { 
+  	//variables    
+	isp = data.client.isp;
+	isp_server = data.server.sponsor;
+	isp_client = data.client.isp; 
+	upload = data.speeds.upload;
+	download = data.speeds.download;  
+	//input to database
+	sql = "INSERT INTO records (isp, isp_server,isp_client,upload,download) VALUES ('" +isp +"','"+isp_server+"','"+isp_client+"','"+upload+"','"+download+"')"; 
+		connection.query(sql, function (err, result) {
+			if (err) throw err;
+	    	console.log("1 record inserted");
 
+	    	//redirect to home
+	    	res.redirect('/');
+		});
+	});
 });
  
 
