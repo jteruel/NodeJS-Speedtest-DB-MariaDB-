0dce0956b4f859888fe49615f080bf19be70194b
Mike <jeffreyteruel@gmail.com>
2019-01-05 17:22:01 +0800

    Update 1/5/19

--- apps/records.js
+++ apps/records.js
@@ -2,15 +2,17 @@ var speedTest = require('speedtest-net');
 var test = speedTest({maxTime: 5000});
 connection = require('../connection.js');
 
-records = connection.connect(function( err) {
+records = connection.connect(function(err) {
 	if (err) throw err; 
-	connection.query("SELECT * FROM records", function (err, result, fields) {
-    	if (err) throw err; 
-    	return result;
-  	});
-}); 
+		connection.query("SELECT * FROM records", function (err, result, fields) {
+    		if (err) throw err; 
+    		return result;
+  		});
+    }); 
+ 
 
-add_record =  test.on('data', data => { 
+add_record =  function() { 
+	test.on('data', data => { 
   	//variables    
 	isp = data.client.isp;
 	isp_server = data.server.sponsor;
@@ -19,11 +21,15 @@ add_record =  test.on('data', data => {
 	download = data.speeds.download;  
 	//input to database
 	sql = "INSERT INTO records (isp, isp_server,isp_client,upload,download) VALUES ('" +isp +"','"+isp_server+"','"+isp_client+"','"+upload+"','"+download+"')"; 
-	connection.query(sql, function (err, result) {
-		if (err) throw err;
-    	console.log("1 record inserted");
+		connection.query(sql, function (err, result) {
+			if (err) throw err;
+	    	console.log("1 record inserted");
+		});
 	});
-});
+};
 
-module.exports = records;
-module.exports = add_record;
+
+module.exports = {
+	records
+};
+ 
--- index.js
+++ index.js
@@ -3,20 +3,20 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 connection = require('./connection.js');
-records = require('./apps/records.js');
+var records = require('./apps/records.js');
 var speedTest = require('speedtest-net');
 var test = speedTest({maxTime: 5000}); 
 
 app.set('view engine', 'ejs');
 // routing
-app.get('/', function (req, res) {
-	res.render('pages/index', { records: records.result });
+app.get('/', function (req, res, records) {
+	console.log(records.records);
+	//res.render('pages/index', records.records);
 });
 
 app.get('/get-record', function(req, res) {
 
-	res.send(records.get_record);
-	//return redirect to homepag
+	//
 
 });
  
